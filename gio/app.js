59e095282b0245cce8458b353b630ebb31f32f05
gio2010 <100205097+gio2010@users.noreply.github.com>
2022-02-22 15:54:15 +0400

    Add files via upload

---
+++ gio/app.js
@@ -0,0 +1,47 @@
+// var ext =document.getElementById('text');
+// Text.style.getElementById.
+
+// var gio = document.querySelector('.giorgi');
+// gio.style.
+
+
+
+
+
+
+// gio.style.backgroundColor = 'red';
+// gio.style.width ='300px';
+// gio.style.height = '300px';
+
+
+
+
+// getElementById('giorgi');
+
+// gio.style.backgroundColor = 'red';
+// gio.style.width ='300px';
+// gio.style.height = '300px';
+// function gio(number) {
+//     var number = 5
+
+//     if (1 > 3) {
+//         console.log(number)
+//     } else {
+//         console.log('arasworea')
+//     }
+// }
+
+// gio()
+
+// function io() {
+//     var h ='giorgi'
+
+//     if (h !== 'giorgi') {
+//        prompt('hello word')
+//     } else {
+//         prompt('hello gio')
+//     }
+// }
+
+// io()
+
